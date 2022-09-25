b0e3640e2fe595fe560ae70c5a65a679840b3ba8
lindakladivova <l.kladivova@seznam.cz>
2022-09-25 06:48:14 -0500

    simplify conf.py, improve github action yml

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,6 @@ extensions = [
     'sphinx.ext.autosummary',  # Create neat summary tables for modules/classes/methods etc
     'sphinx.ext.intersphinx',  # Link to other project's documentation (see mapping below)
     'sphinx.ext.viewcode',  # Add a link to the Python source code for classes, functions etc.
-    'sphinx_autodoc_typehints', # Automatically document param types (less noise in class signature)
     'nbsphinx',  # Integrate Jupyter Notebooks and Sphinx
     'sphinx_copybutton',
     'IPython.sphinxext.ipython_console_highlighting'
