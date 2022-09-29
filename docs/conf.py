43b8837e24dd42b2cf9f69449481722ea5d782f1
Linda Kladivova <49241681+lindakladivova@users.noreply.github.com>
2022-09-29 16:30:23 +0200

    sphinx_copybutton not needed

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,6 @@ release = '1.0'
 # ones.
 extensions = [
     'nbsphinx',  # Integrate Jupyter Notebooks and Sphinx
-    'sphinx_copybutton'
 ]
 
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
