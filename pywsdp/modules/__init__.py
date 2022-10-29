e4636fcc3a83f67b020cd0ba889e4463d56dd89b
Linda Kladivova <49241681+lindakladivova@users.noreply.github.com>
2022-10-29 09:03:23 +0200

    Inheritance diagram - public API (#39)
    * added inheritance diagram of modules to public API

--- pywsdp/modules/__init__.py
+++ pywsdp/modules/__init__.py
@@ -1,3 +1,18 @@
+"""
+    pywsdp.modules
+
+    .. inheritance-diagram::
+            pywsdp.base.WSDPBase
+            pywsdp.base.SestavyBase
+            pywsdp.modules.CtiOS
+            pywsdp.modules.GenerujCenoveUdajeDleKu
+            pywsdp.modules.SeznamSestav
+            pywsdp.modules.VratSestavu
+            pywsdp.modules.SmazSestavu
+       :parts: 1
+
+"""
+
 from pywsdp.modules.CtiOS import CtiOS
 from pywsdp.modules.CtiOS import formats
 from pywsdp.modules.GenerujCenoveUdajeDleKu import GenerujCenoveUdajeDleKu
